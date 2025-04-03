f9bb236e83ca08058073b3959a019ac2073aec5d
PremPatel1010 <premrpatel2007@gmail.com>
2025-04-03 13:52:31 +0530

    Deploy-03-02

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,18 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
-// https://vite.dev/config/
 export default defineConfig({
+  plugins: [react(), tailwindcss()],
   base: '/',
   build: {
     outDir: '../backend/public',
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html')
+      }
+    }
   }
-});
+})
